296bb24cf37157f6990cda9463c80021d86d1f0e
Santiago Torres Martínez <santitorresm0@gmail.com>
2026-05-28 12:08:32 -0500

    Translate MVC documentation comments to English - module-info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,7 @@ module com.example.demo2 {
     requires javafx.controls;
     requires javafx.fxml;
 
-    // Exportar los paquetes para que javafx.fxml pueda acceder al controlador
+    // Export packages so that javafx.fxml can access the controller
     opens com.example.demo2 to javafx.graphics, javafx.fxml;
     opens com.example.demo2.controller to javafx.fxml;
     opens com.example.demo2.view to javafx.fxml;
